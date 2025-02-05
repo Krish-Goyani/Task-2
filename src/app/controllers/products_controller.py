23572efee1428f3fd4e317f913e4ba146f5df878
Krish-Goyani <goyani@duck.com>
2025-02-05 22:53:53 +0530

    feat:  products/{products_id} end point added

--- src/app/controllers/products_controller.py
+++ src/app/controllers/products_controller.py
@@ -1,5 +1,6 @@
 from fastapi import Depends
 from src.app.usecases.products_usecase import ProductsUseCases
+from src.app.model.schemas.product_schemas import Product
 
 
 class ProductsController:
@@ -9,3 +10,12 @@ class ProductsController:
     async def preload_products(self) -> str:
         result = await self.products_usecases.preload_products()
         return result
+    
+    async def add_product(self, product : Product):
+        return await self.products_usecases.add_product_usecase(product)
+    
+    async def fetch_all_products_controller(self):
+        return await self.products_usecases.fetch_all_products_usecase()
+    
+    async def products_details_controller(self, product_id):
+        return await self.products_usecases.products_details_usecase(product_id)
